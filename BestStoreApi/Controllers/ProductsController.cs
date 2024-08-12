8dff899970ecc3f0c87b08cbef423b2f5e8ee9af
batuhanlog <mrkayahan66@gmail.com>
2024-08-12 15:07:33 +0300

    Add product/categoris items

--- BestStoreApi/Controllers/ProductsController.cs
+++ BestStoreApi/Controllers/ProductsController.cs
@@ -1,9 +1,10 @@
 ﻿using DigitalStore.Core.DTOs;
 using DigitalStore.Core.Models;
 using DigitalStore.Repository.Context;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace DigitalStore.API.Controllers
 {
@@ -12,36 +13,37 @@ namespace DigitalStore.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly IWebHostEnvironment env;
 
-        public ProductsController(ApplicationDbContext context, IWebHostEnvironment env)
+        public ProductsController(ApplicationDbContext context)
         {
             _context = context;
-            this.env = env;
         }
 
         [HttpGet]
-        public IActionResult GetProducts(string? search, string? category,
+        public IActionResult GetProducts([FromQuery] string? token, string? search, string? category,
                                          int? minPrice, int? maxPrice,
                                          string? sort, string? order,
                                          int? page, bool? isAvailable)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             IQueryable<Product> query = _context.Products.Include(p => p.ProductCategories)
                                                          .ThenInclude(pc => pc.Category);
 
-            // Arama işlevselliği
+            // Filtreleme ve sıralama işlemleri
             if (search != null)
             {
                 query = query.Where(p => p.Name.Contains(search) || p.Description.Contains(search));
             }
 
-            // Kategoriye göre filtreleme
             if (category != null)
             {
                 query = query.Where(p => p.ProductCategories.Any(pc => pc.Category.Name == category));
             }
 
-            // Fiyat aralığı filtreleme
             if (minPrice != null)
             {
                 query = query.Where(p => p.Price >= minPrice);
@@ -52,13 +54,11 @@ namespace DigitalStore.API.Controllers
                 query = query.Where(p => p.Price <= maxPrice);
             }
 
-            // Stok durumu filtreleme
             if (isAvailable.HasValue)
             {
                 query = query.Where(p => p.IsAvailable == isAvailable.Value);
             }
 
-            // Sıralama işlevselliği
             if (sort == null) sort = "id";
             if (order == null || order != "asc") order = "desc";
 
@@ -84,7 +84,7 @@ namespace DigitalStore.API.Controllers
                     break;
             }
 
-            // Sayfalama işlevselliği
+            // Sayfalama işlemi
             if (page == null || page < 1) page = 1;
 
             int pageSize = 5;
@@ -94,7 +94,7 @@ namespace DigitalStore.API.Controllers
 
             var products = query.ToList();
 
-            var response = new
+            var result = new
             {
                 Products = products,
                 TotalPages = totalPages,
@@ -102,13 +102,38 @@ namespace DigitalStore.API.Controllers
                 Page = page
             };
 
-            return Ok(response);
+            return Ok(result);
+        }
+
+        [HttpGet("by-category/{categoryName}")]
+        public IActionResult GetProductsByCategory([FromQuery] string token, string categoryName)
+        {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
+            var products = _context.Products.Include(p => p.ProductCategories)
+                                            .ThenInclude(pc => pc.Category)
+                                            .Where(p => p.ProductCategories.Any(pc => pc.Category.Name == categoryName))
+                                            .ToList();
+
+            if (!products.Any())
+            {
+                return NotFound($"Category '{categoryName}' does not exist or has no products.");
+            }
+
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        //[Authorize(Roles = "admin")]
-        public IActionResult GetProduct(int id)
+        public IActionResult GetProduct([FromQuery] string token, int id)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             var product = _context.Products.Include(p => p.ProductCategories)
                                            .ThenInclude(pc => pc.Category)
                                            .FirstOrDefault(p => p.Id == id);
@@ -121,10 +146,14 @@ namespace DigitalStore.API.Controllers
             return Ok(product);
         }
 
-        //[Authorize(Roles = "admin")]
         [HttpPost]
-        public async Task<IActionResult> CreateProduct([FromForm] ProductDto productDto)
+        public async Task<IActionResult> CreateProduct([FromQuery] string token, [FromForm] ProductDto productDto)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             var product = new Product
             {
                 Name = productDto.Name,
@@ -132,14 +161,14 @@ namespace DigitalStore.API.Controllers
                 Price = productDto.Price,
                 Description = productDto.Description,
                 CreatedAt = DateTime.UtcNow,
-                StockQuantity = productDto.StockQuantity, // Stok miktarı
-                IsAvailable = productDto.IsAvailable // Ürün durumu
+                StockQuantity = productDto.StockQuantity,
+                IsAvailable = productDto.IsAvailable
             };
 
             var category = await _context.Categories.FindAsync(productDto.CategoryId);
             if (category == null)
             {
-                return BadRequest("Geçersiz Kategori ID'si.");
+                return BadRequest("Invalid Category ID.");
             }
 
             product.ProductCategories.Add(new ProductCategory
@@ -154,10 +183,14 @@ namespace DigitalStore.API.Controllers
             return Ok(product);
         }
 
-        //[Authorize(Roles = "admin")]
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateProduct(int id, [FromForm] ProductDto productDto)
+        public async Task<IActionResult> UpdateProduct([FromQuery] string token, int id, [FromForm] ProductDto productDto)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             var product = await _context.Products.Include(p => p.ProductCategories)
                                                  .FirstOrDefaultAsync(p => p.Id == id);
             if (product == null)
@@ -169,15 +202,15 @@ namespace DigitalStore.API.Controllers
             product.Brand = productDto.Brand;
             product.Price = productDto.Price;
             product.Description = productDto.Description ?? "";
-            product.StockQuantity = productDto.StockQuantity; // Stok miktarını güncelle
-            product.IsAvailable = productDto.IsAvailable; // Ürün durumunu güncelle
+            product.StockQuantity = productDto.StockQuantity;
+            product.IsAvailable = productDto.IsAvailable;
 
             product.ProductCategories.Clear();
 
             var category = await _context.Categories.FindAsync(productDto.CategoryId);
             if (category == null)
             {
-                return BadRequest("Geçersiz Kategori ID'si.");
+                return BadRequest("Invalid Category ID.");
             }
 
             product.ProductCategories.Add(new ProductCategory
@@ -191,10 +224,14 @@ namespace DigitalStore.API.Controllers
             return Ok(product);
         }
 
-        //[Authorize(Roles = "admin")]
         [HttpPut("update-stock/{id}")]
-        public async Task<IActionResult> UpdateStock(int id, int stockQuantity)
+        public async Task<IActionResult> UpdateStock([FromQuery] string token, int id, int stockQuantity)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             var product = await _context.Products.FindAsync(id);
             if (product == null)
             {
@@ -207,10 +244,14 @@ namespace DigitalStore.API.Controllers
             return Ok(product);
         }
 
-        //[Authorize(Roles = "admin")]
         [HttpDelete("{id}")]
-        public IActionResult DeleteProduct(int id)
+        public IActionResult DeleteProduct([FromQuery] string token, int id)
         {
+            if (!IsTokenValid(token, out var response))
+            {
+                return response;
+            }
+
             var product = _context.Products.Find(id);
 
             if (product == null)
@@ -222,6 +263,38 @@ namespace DigitalStore.API.Controllers
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        private bool IsTokenValid(string? token, out IActionResult? response)
+        {
+            response = null;
+
+            if (string.IsNullOrEmpty(token))
+            {
+                response = Unauthorized("Token is required.");
+                return false;
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken? jwtToken;
+
+            try
+            {
+                jwtToken = handler.ReadToken(token) as JwtSecurityToken;
+            }
+            catch (Exception)
+            {
+                response = Unauthorized("Invalid token.");
+                return false;
+            }
+
+            if (jwtToken == null)
+            {
+                response = Unauthorized("Invalid token.");
+                return false;
+            }
+
+            return true;
         }
     }
 }
